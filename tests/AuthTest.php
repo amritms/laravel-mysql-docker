c3c7149a66459b2fe4827fbf8da01350d5b166fc
Amrit Man Shrestha <amritms@gmail.com>
2015-11-17 00:18:23 +0545

    write test for user register

---
+++ tests/AuthTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+
+    function a_user_may_register_for_an_account_but_must_confirm_their_email_address(){
+        $this->visit('register')
+            ->type('JohnDoe', 'name')
+            ->type('johndoe@example.com', 'email')
+            ->type('password123', 'password')
+            ->press('Register');
+
+        $this->see('Please confirm your email address')
+            ->seeInDatabase('users', ['name' => 'JohnDoe', 'verified' => 0]);
+
+    }
+}
