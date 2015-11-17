f4652c0f0613267bfafd6d79b1b9b0b2dc253bc6
Amrit Man Shrestha <amritms@gmail.com>
2015-11-17 23:44:24 +0545

    adds confirm registration test

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class AuthTest extends TestCase
 {
@@ -21,5 +22,18 @@ class AuthTest extends TestCase
         $this->see('Please confirm your email address.')
             ->seeInDatabase('users', ['name' => 'JohnDoe', 'verified' => 0]);
 
+        $user = User::whereName('JohnDoe')->first();
+
+//        $this->login($user)->see('Could not sign you in.');
+
+        $this->visit("register/confirm/{$user->token}")
+            ->see('You are now confirmed. Please login.')
+            ->seeInDatabase('users', ['name' => 'JohnDoe', 'verified' => 1]);
+
+    }
+
+    protected function login($user = null)
+    {
+        $user = $user ?: $this->factory->create('App\User', ['password' => 'newPassword']);
     }
 }
