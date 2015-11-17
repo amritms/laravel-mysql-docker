53a0791164311da694ae18e52d09aa6d1e3f9aaa
Amrit Man Shrestha <amritms@gmail.com>
2015-11-17 17:54:32 +0545

    registration controller

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+//use App\Mailers\AppMailer;
+
+use App\Http\Controllers\Controller;
+
+class RegistrationController extends Controller
+{
+    public function Register()
+    {
+        return view('auth.register');
+    }
+
+    public function PostRegister(Request $request)
+    {
+        $this->validate($request, [
+           'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create($request->all());
+
+//        $mailer->sendEmailConfirmationTo($user);
+
+        flash('Please confirm your email address.');
+
+        return redirect()->back();
+
+    }
+}
