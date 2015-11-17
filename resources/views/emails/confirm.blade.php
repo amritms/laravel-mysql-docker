2da83374e4846f35801bf772f7f18354a1161760
Amrit Man Shrestha <amritms@gmail.com>
2015-11-17 17:54:03 +0545

    few views

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sam
- * Date: 11/17/15
- * Time: 5:20 PM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Signup Confirmation</title>
+</head>
+<body>
+<h1>Thanks for siging up</h1>
+<p> we just want you to <a href="{{ url('register/confirm/{$user->token}') }}">confirm your email address</a></p>
+</body>
+</html>
