21a0f22fc30342954fc7216d7dbcf8d4939bc1f3
Amrit Man Shrestha <amritms@gmail.com>
2015-11-22 20:16:09 +0545

    adds remember me code. Also fixes redirection after logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,7 +21,7 @@ class SessionsController extends Controller
            'password' => 'required'
         ]);
 
-        if(Auth::attempt($this->getCredentials($request)))
+        if(Auth::attempt($this->getCredentials($request), $request->remember))
         {
             flash('Welcome!');
             return redirect()->intended('/admin');
@@ -36,7 +36,7 @@ class SessionsController extends Controller
     {
         Auth::logout();
         flash('You have now been signed out.');
-        redirect('login');
+        return redirect('login');
     }
 
     protected function getCredentials(Request $request)
